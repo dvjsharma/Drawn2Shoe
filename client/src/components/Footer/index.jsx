5dc717d20b2e47c10f1f0d4eed4d142ddaa7949e
Divij Sharma <divijs75@gmail.com>
2023-10-01 02:27:28 +0530

    made components responsive and added hamburger menu

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import logo from '../../assets/logo-final.png'
 
 const Footer = () => {
   return (
     <div className='flex items-center justify-center w-full flex-col'>
         <div className='flex items-center justify-center w-[80%] flex-wrap min-h-[6rem] max-lg:flex-col font-bold'>
-            <div className='flex items-center w-[20%] text-3xl flex-wrap my-3 max-lg:justify-center'>
-                Drawn2Shoe
+            <div className='flex items-center w-[20%] text-3xl flex-wrap my-3 max-lg:justify-center max-lg:w-full'>
+                <img src={logo} alt="" className='h-9 w-48' />
             </div>
             <div className='flex items-center justify-center w-[60%] text-[15px] flex-wrap gap-4 my-3'>
                 <NavLink to="/">Home</NavLink>
--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,11 +1,58 @@
 import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import {Divide, Divide as Hamburger} from 'hamburger-react'
+import {stack as Menu} from 'react-burger-menu'
+import logo from '../../assets/logo-final.png'
 
 const Navbar = () => {
+  var styles = {
+    bmBurgerButton: {
+      display: 'none',
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px'
+    },
+    bmCross: {
+      background: '#bdc3c7'
+    },
+    bmMenuWrap: {
+      position: 'fixed',
+      height: '100%'
+    },
+    bmMenu: {
+      background: 'white',
+      padding: '2.5em 1.5em 0',
+      fontSize: '1.15em'
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      display: 'flex',
+      flexDirection: 'column',
+      color: 'black',
+      padding: '0.8em'
+    },
+    bmItem: {
+      display: 'inline-block',
+      padding: '0.5em'
+    },
+    bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)'
+    }
+  }
   const [toggled, setToggled] = useState(false)
   return (
-    <nav className="flex items-center p-2 text-sm">
+    <>
+    <Menu right isOpen={toggled} styles={styles} onClose={()=>(setToggled(false))} >
+      <Link to="/" className='menu-item'>Home</Link>
+      <Link to="categories" className='menu-item' onClick={()=>{setToggled(false)}} >Categories</Link>
+      <Link to="customize" className='menu-item' onClick={()=>{setToggled(false)}} >Customize</Link>
+      <Link to="shop" className='menu-item' onClick={()=>{setToggled(false)}} >Shop</Link>
+      <Link to="login" className='menu-item' onClick={()=>{setToggled(false)}} >Login</Link>
+    </Menu>
+    <nav className="flex items-center py-4 px-2 text-sm">
       <div className="flex gap-4 w-[30%] justify-center items-center max-lg:hidden">
         <NavLink
           to="/"
@@ -34,12 +81,12 @@ const Navbar = () => {
           Shop
         </NavLink>
       </div>
-      <div className="flex w-[50%] text-left justify-center items-center">
+      <div className="flex w-[50%] text-left justify-center items-center max-lg:w-full max-lg:justify-start">
         <div className='lg:hidden'>
           <Divide size={20} toggled={toggled} toggle={setToggled} />
         </div>
         <Link to="/" className="font-bold text-[25px]">
-          Drawn2Shoe
+          <img src={logo} alt="" className='w-40'/>
         </Link>
       </div>
       <div className="flex w-[30%] justify-center gap-4 max-lg:w-full max-lg:justify-end">
@@ -55,6 +102,7 @@ const Navbar = () => {
       </button> </Link>
       </div>
     </nav>
+    </>
   );
 }
 

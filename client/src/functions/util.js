e4375e70e286d07cc371975057b30241c017f6dd
Divij Sharma <divijs75@gmail.com>
2023-09-30 21:04:53 +0530

    initial setup

---
+++ client/src/functions/util.js
@@ -0,0 +1,14 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState ={
+    
+}
+
+export const todoSlice = createSlice({
+    // name: 
+    // state:
+    // reducers:
+})
+
+// export const {} =util.actions
+// export default util.reducer

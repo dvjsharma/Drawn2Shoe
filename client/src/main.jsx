b458d5af3d22da1fbe17e1008a0b0df5a408afa6
Divij Sharma <divijs75@gmail.com>
2023-09-30 21:12:45 +0530

    redux store setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './manager/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 )

ef9ece6487fa20e45af7d24f388e417db6e326ed
Akash Kumar Sah <akashsah2003@gmail.com>
2023-10-01 06:13:44 +0530

    Footer added

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 const Layout = () => {
   return (
     <>
         <Navbar/>
         <Outlet/>
-        {/* <Footer/> */}
+        <Footer/>
     </>
   )
 }

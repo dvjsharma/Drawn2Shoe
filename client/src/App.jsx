011ba0e0326fd24fac4b0b657f66856a2bb06d71
Akash Kumar Sah <akashsah2003@gmail.com>
2023-10-01 06:08:58 +0530

    Footer made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Test from "./components/test"
+import Home from "./pages/Home"
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test/>}/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   )

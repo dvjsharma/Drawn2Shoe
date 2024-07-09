07d6ebc3955dce0b9301d05019f7ebdcdae2ff79
Divij Sharma <divijs75@gmail.com>
2024-07-10 00:59:34 +0530

    fix: Fixed preloader bad import

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar/navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import Preloader from "./preloader/Preloader";
+import Preloader from "./Preloader/Preloader";
 
 const Layout = () => {
     return (
